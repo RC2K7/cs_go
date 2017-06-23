370bb895958b559b35bbefe7bba60909c83420a4
Miguel Saldivar <miguel.saldivar22@hotmail.com>
2017-06-23 09:57:16 -0700

    Nothing really

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-
-export default () => (
-    
+export default () => {
+  return(
     <div>
       <h1> Hello from react and Express!</h1>
-      <input type="search" id="search" placeholder="Search..."/>
     </div>
-);
+  )
+};
+
+
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ let app = express();
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(3000, () => console.log("http://localhost:3000"));
