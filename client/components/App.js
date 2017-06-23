14469257c92b5dbf61abc2a6a7f4f27ddfb86534
Miguel Saldivar <miguel.saldivar22@hotmail.com>
2017-06-23 10:39:58 -0700

    added hot reloading, meaning it the application gets reloaded whenever something gets saved

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-export default () => {
-  return(
-    <div>
-      <h1> Hello from react and Express!</h1>
-    </div>
-  )
+class App extends React.Component{
+  render() {
+    return (
+      <h1> Hello from {this.props.name}</h1>
+    );
+  };
 };
+
+export default App;
+
 
 
